c53377c59f74877ab1ebca89ba6ec06a4f071673
DELL <DELL@DESKTOP-HUP7IRF>
2020-06-24 22:22:00 +0530

    Updated

--- MileStone1/src/com/wipro/LanguageBasics/Ex_1.java
+++ MileStone1/src/com/wipro/LanguageBasics/Ex_1.java
@@ -1,5 +1,8 @@
 package com.wipro.LanguageBasics;
 
 public class Ex_1 {
+	public static void main(String args[]) {
+		System.out.println(args[0]+" Technologies "+args[1]);
+	}
 
 }
--- MileStone1/src/com/wipro/LanguageBasics/Ex_2.java
+++ MileStone1/src/com/wipro/LanguageBasics/Ex_2.java
@@ -1,5 +1,8 @@
 package com.wipro.LanguageBasics;
 
 public class Ex_2 {
-
+	public static void main(String args[])
+	{
+		System.out.println("Welcome "+ args[0]);
+	}
 }
